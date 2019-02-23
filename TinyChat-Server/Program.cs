406400b5e51e7d33a8ba77ac819d4dd7ff313055
nmar1249 <37256515+nmar1249@users.noreply.github.com>
2019-02-22 18:24:01 -0600

    Finished the server component

--- TinyChat-Server/Program.cs
+++ TinyChat-Server/Program.cs
@@ -22,6 +22,7 @@ namespace TinyChat_Server
             Program domain = new Program();
             domain.clients = new List<ClientHandler>();
 
+            //arguments are the ip and port you wish to connect to, default is any on port 8000
             if(args.Length == 0)
             {
                 domain.serverPort = 8000;
@@ -40,6 +41,7 @@ namespace TinyChat_Server
                 domain.serverIP = IPAddress.Parse(args[0]);
             }
 
+            //init listener and start the server
             domain.Listener_bw = new BackgroundWorker();
             domain.Listener_bw.WorkerSupportsCancellation = true;
             domain.Listener_bw.DoWork += new DoWorkEventHandler(domain.Start);
@@ -55,10 +57,118 @@ namespace TinyChat_Server
         private void Start(object sender, DoWorkEventArgs e)
         {
             //finish later
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenerSocket.Bind(new IPEndPoint(serverIP, serverPort));
+            listenerSocket.Listen(200);
+
+            while (true)
+                CreateClientHandler(listenerSocket.Accept());
         }
-        private string SetManagerName(IPAddress remoteIP, string name)
+
+        private void CreateClientHandler(Socket socket)
         {
-            //finish this later
+            ClientHandler newHandler = new ClientHandler(socket);
+            newHandler.CommandReceived += new CommandReceivedEventHandler(CommandReceived);
+            newHandler.Disconnected += new DisconnectedEventHandler(ClientDC);
+            AbnormalCheck(newHandler);
+            clients.Add(newHandler);
+            UpdateConsole("Connected.", newHandler.IP, newHandler.Port);
+
+        }
+        private void AbnormalCheck(ClientHandler handler)
+        {
+            if (RemoveClient(handler.IP))
+                UpdateConsole("Disconnected:", handler.IP, handler.Port);
+        }
+        void ClientDC(object sender, ClientEventArgs e)
+        {
+            if (RemoveClient(e.IP))
+                UpdateConsole("Disconnected: ", e.IP, e.Port);
+        }
+        private bool RemoveClient(IPAddress ip)
+        {
+            lock(this)
+            {
+                int i = ClientIndex(ip);
+                if(i != -1)
+                {
+                    string n = clients[i].ClientName;
+                    clients.RemoveAt(i);
+
+                    Command c = new Command(Type.ClientLogoff, IPAddress.Broadcast);
+                    c.SenderName = n;
+                    c.SenderIP = ip;
+                    BroadcastCommand(c);
+                    return true;
+                }
+                return false;
+            }
+        }
+        private void CommandReceived(object sender, CommandEventArgs e)
+        {
+            if (e.Cmd.CmdType == Type.ClientLogin)
+                SetHandlerName(e.Cmd.SenderIP, e.Cmd.Body);
+
+            if (e.Cmd.CmdType == Type.IsNameExists)
+            {
+                bool isExists = DoesNameExist(e.Cmd.SenderIP, e.Cmd.Body);
+                Existance(e.Cmd.SenderIP, isExists);
+                return;
+            }
+            else if (e.Cmd.CmdType == Type.SendClientList)
+            {
+                SendClientList(e.Cmd.SenderIP);
+                return;
+            }
+
+            if (e.Cmd.Target.Equals(IPAddress.Broadcast))
+                BroadcastCommand(e.Cmd);
+            else
+                SendCommandToTarget(e.Cmd);
+        }
+        private void Existance(IPAddress targetIP, bool isExists)
+        {
+            Command cExists = new Command(Type.IsNameExists, targetIP, isExists.ToString());
+            cExists.SenderIP = serverIP;
+            cExists.SenderName = "SERVER";
+            SendCommandToTarget(cExists);
+        }
+        //sends the list of clients to a newly connected client
+        private void SendClientList(IPAddress newIP)
+        {
+            foreach(ClientHandler handler in clients)
+            {
+                if(handler.Connected && !handler.IP.Equals(newIP))
+                {
+                    Command c = new Command(Type.SendClientList, newIP);
+                    c.Body = handler.IP.ToString() + ":" + handler.ClientName;
+                    c.SenderIP = serverIP;
+                    c.SenderName = "SERVER";
+                    SendCommandToTarget(c);
+                }
+            }
+        }
+        //gets the client index of specified IP; returns -1 if not found
+        private int ClientIndex(IPAddress ip)
+        {
+            int i = -1;
+            foreach(ClientHandler handler in clients)
+            {
+                i++;
+                if (handler.IP.Equals(ip))
+                    return i;
+            }
+            return -1;
+        }
+        private string SetHandlerName(IPAddress remoteIP, string name)
+        {
+            int i = ClientIndex(remoteIP);
+            if(i != -1)
+            {
+                string n = name.Split(new char[] { ':' })[1];
+                clients[i].ClientName = n;
+                return n;
+            }
             return "";
         }
         private bool DoesNameExist(IPAddress remoteIP, string name)
